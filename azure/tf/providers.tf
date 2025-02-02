eb7bd575386561ba29715e923a671f84cd8a8cde
Lukas Plaehn <28509449+lukasplaehn@users.noreply.github.com>
2025-02-02 13:53:32 +0100

    Added cloud block to connect to the remote HCP Terraform workspace

--- azure/tf/providers.tf
+++ azure/tf/providers.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "registry.terraform.io/hashicorp/azurerm"
+      version = "4.16.0" # Developed when at Latest Version == 4.16.0
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = ">=1.52.0"
+    }
+  }
+  cloud {
+    organization = "Lukas-Plaehn"
+    workspaces {
+      name = "databricks-sra"
+    }
+    hostname = "app.terraform.io"
+    token = var.terraform_token
+  }
+}
+
 provider "azurerm" {
   features {}
 
